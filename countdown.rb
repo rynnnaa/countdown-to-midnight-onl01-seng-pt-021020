96d1aefe7179a5ebf69a6646fd12a01f21dbd00e
Ryna Truong <rynatruong@outlook.com>
2020-02-20 06:22:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(int)
   int = 0
   while int < 10
-  puts "#{int} SECOND(S)!"
-  "HAPPY NEW YEAR!"
-  int -= 1
-end
+    puts "#{int} SECOND(S)!"
+    "HAPPY NEW YEAR!"
+    int -= 1
+  end
 end
